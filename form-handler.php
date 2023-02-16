d0a1538c87be10ff3d21eeed681a0182722676d3
patrickateng <99479428+patrickateng@users.noreply.github.com>
2023-02-16 17:42:04 +0300

    third commit

---
+++ form-handler.php
@@ -0,0 +1,8 @@
+<?php
+$_name = $_POST['name'];
+$_visitor_email = $_POST['email'];
+$_subject = $_POST['subject'];
+$_message = $_POST['message'];
+
+$email_from = 'enter@yourwebsiteid_here.com'
+?>
