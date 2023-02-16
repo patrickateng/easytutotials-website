940ef3305466408586991ba29dd638d77d7cd182
patrickateng <99479428+patrickateng@users.noreply.github.com>
2023-02-16 18:01:46 +0300

    third commit

--- form-handler.php
+++ form-handler.php
@@ -4,5 +4,20 @@ $_visitor_email = $_POST['email'];
 $_subject = $_POST['subject'];
 $_message = $_POST['message'];
 
-$email_from = 'enter@yourwebsiteid_here.com'
+$email_from = 'enter@yourwebsiteid(domain_name)_here.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+               "Subject: $subject.\n".
+               "User Message: $message.\n";
+
+$to = "wheretorecieveinquiry@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
 ?>
